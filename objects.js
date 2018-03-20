51e009680d415ceaf50db9d00f180414a904aacd
acherone92@gmail.com <acherone92@gmail.com>
2018-03-20 02:51:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName:'songTitle'};
 
-function updatePlaylist (playlist, artist,song){
+function updatePlaylist (playlist, artistName, songTitle){
   playlist.artist=song;
   return playlist;
 }
